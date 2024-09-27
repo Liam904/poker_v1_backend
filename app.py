f95c290af52a8b4e7deee7516f8f2eecda6187d0
Liam904 <lw433891@gmail.com>
2024-09-27 15:27:59 +0300

    'waitress'

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from app import create_app
+from waitress import serve
+from app import create_app  # Adjust the import as necessary
 
 app = create_app()
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    serve(app, host="0.0.0.0", port=8080)
--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,30 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
 import os
 
+db = SQLAlchemy()
 bcrypt = Bcrypt()
 jwt = JWTManager()
 migrate = Migrate()
-db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
-
-    # Load configurations from environment variables
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
         "DATABASE_URL", "sqlite:///game.db"
     )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "default_secret")
 
-    # Initialize extensions with the app
     db.init_app(app)
     bcrypt.init_app(app)
     migrate.init_app(app, db)
-    jwt.init_app(app)
-    CORS(app)  # You can specify origins if needed
+    CORS(app)
 
-    # Register blueprints
     from .views import views
     from .auth_routes import auth
 
